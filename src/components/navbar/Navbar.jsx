6a3e61bb26225478334f5112f9c767087752fce7
imnrb <67147637+imnrb@users.noreply.github.com>
2022-02-27 19:48:57 +0530

    navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,24 +4,26 @@ import './navbar.css'
 import { FaGithub } from 'react-icons/fa';
 const Navbar = () => {
   return (
-    <div div className="i">
-     <nav className='main-nav'>
-        <div className='logo'>
-          <img src={me} alt="" />
-        </div>
-        <div className='menu-link'>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="https://github.com/imnrb" target="_" >
-                   <FaGithub/>
-                </a>
-                </li>
-            </ul>
-        </div>
-     </nav>
+    <div className='n'>
+    
+      <nav className='main-nav'>
+         <div className='logo'>
+           <img src={me} alt="" />
+         </div>
+         <div className='menu-link'>
+             <ul>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">About</a></li>
+                 <li><a href="#">Services</a></li>
+                 <li><a href="#">Contact</a></li>
+                 <li><a href="https://github.com/imnrb" target="_" >
+                    <FaGithub/>
+                 </a>
+                 </li>
+             </ul>
+         </div>
+      </nav>
+  
     </div>
   )
 }
