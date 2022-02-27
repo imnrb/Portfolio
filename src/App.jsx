fc052a9bff368f0e5d48c3722bba2121b7b08358
imnrb <67147637+imnrb@users.noreply.github.com>
2022-02-27 11:08:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Intro from "./components/intro/Intro";
+import About from "./components/about/About"
+const App = () => {
+  return <div><Intro/>
+  
+  <About/></div>;
+};
+
+export default App;
